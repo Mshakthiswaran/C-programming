34ef0e268bf6bc246b858ce41c95ad47b687c7d6
Mshakthiswaran <168906686+Mshakthiswaran@users.noreply.github.com>
2025-07-13 11:02:59 +0530

    Add files via upload

---
+++ main.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    // ARITHMETIC OPERATER
+    int x = 10,y=2,z=0;
+    z=x+y;
+   /* z=x-y;
+    z=x*y;
+    z=x/y;
+    z=x%y;
+    x++;
+    x--;*/
+     printf("the addition is %d",z);
+    return 0;
+}
