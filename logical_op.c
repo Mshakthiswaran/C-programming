c646f5e46f9a56765bc2bd8d9aae36bed41ee937
Mshakthiswaran <168906686+Mshakthiswaran@users.noreply.github.com>
2025-07-16 19:03:26 +0530

    Add files via upload

---
+++ logical_op.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+int main(){
+
+        //logical operator - used to combine or modify boolean expression
+        //&& - and
+        //|| - or
+        //!  - not 
+
+        int temp =0;
+        if(temp>0 && temp <30){
+            printf("the temperature is GOOD");
+        }else{
+             printf("the temperature is bad");
+        }
+
+        
+}
